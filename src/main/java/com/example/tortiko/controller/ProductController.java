53bf36448cf13df416f8adb7efe8ec0d7c05a9e7
Andrei Korotkov <akorotkov1497@gmail.com>
2020-08-10 13:43:06 +0200

    Upload controller works properly

--- src/main/java/com/example/tortiko/controller/ProductController.java
+++ src/main/java/com/example/tortiko/controller/ProductController.java
@@ -3,12 +3,10 @@ package com.example.tortiko.controller;
 import com.example.tortiko.model.Product;
 import com.example.tortiko.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class ProductController {
@@ -19,12 +17,17 @@ public class ProductController {
     public ProductController(ProductRepository productRepository){
         this.productRepository = productRepository;
     }
+    @GetMapping("{id}")
+    public Product getOne(@PathVariable("id") Product product){
+        return product;
+    }
     @RequestMapping(value = "/get-products", method = RequestMethod.GET)
     public List<Product> productList(){
         return productRepository.findAll();
     }
     @RequestMapping(value = "/upload-product", method = RequestMethod.POST)
     public Product createProduct(@RequestBody Product product)  {
+        product.setId(new Random().nextLong());
         return productRepository.save(product);
     }
 }
--- src/main/java/com/example/tortiko/model/Product.java
+++ src/main/java/com/example/tortiko/model/Product.java
@@ -3,53 +3,76 @@ package com.example.tortiko.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name="products", schema = "products")
+@Table(name="products", schema = "public")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String productName;
-    private String productDescription;
-    private String productPhotoUrl;
-    private int productPrice;
-    private String size;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-    public int getId() {
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_description")
+    private String productDescription;
+
+    @Column(name = "product_photo_url")
+    private String productPhotoUrl;
+
+    @Column(name = "product_price")
+    private int productPrice;
+
+    @Column(name = "product_size")
+    private String productSize;
+
+    public Long getId() {
         return id;
     }
-    public void setId(int id) {
+
+    public void setId(Long id) {
         this.id = id;
     }
+
     public String getProductName() {
         return productName;
     }
+
     public void setProductName(String productName) {
         this.productName = productName;
     }
+
     public String getProductDescription() {
         return productDescription;
     }
+
     public void setProductDescription(String productDescription) {
         this.productDescription = productDescription;
     }
+
     public String getProductPhotoUrl() {
         return productPhotoUrl;
     }
+
     public void setProductPhotoUrl(String productPhotoUrl) {
         this.productPhotoUrl = productPhotoUrl;
     }
+
     public int getProductPrice() {
         return productPrice;
     }
+
     public void setProductPrice(int productPrice) {
         this.productPrice = productPrice;
     }
-    public String getSize() {
-        return size;
+
+    public String getProductSize() {
+        return productSize;
     }
-    public void setSize(String size) {
-        this.size = size;
+
+    public void setProductSize(String productSize) {
+        this.productSize = productSize;
     }
 }
+
 
 
