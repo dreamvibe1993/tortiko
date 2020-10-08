7f319442e3c151292f142544ddc26b1af81f0f76
dreamvibe1993 <dreamvibe@mail.ru>
2020-10-08 22:51:07 +0500

    Login.js's updated!
    Cheers m8

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,27 +1,30 @@
 
-const loginEmail = document.getElementById('login_email');
-const loginPassword = document.getElementById("login_password");
+const loginEmail = document.getElementById('username');
+const loginPassword = document.getElementById("password")
 const loginSubmitButton = document.getElementById('login-btn');
 
-const emailInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const passwordInputConfirm = document.getElementById("register_confirm_password");
+const emailInput = document.getElementById('register_email');
+const passwordInput = document.getElementById("register_password")
+const passwordInputConfirm = document.getElementById("register_confirm_password")
 const registerSubmitButton = document.getElementById('register-btn');
-function validatePassword(){
-  if (passwordInput.value != passwordInputConfirm.value) {
-    register_confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-      register_confirm_password.setCustomValidity('');
-  }
 
-  if (!registerCheckbox.checked) {
-    registerCheckbox.setCustomValidity("The checkbox's nescessary");
-  } else {
-      registerCheckbox.setCustomValidity('');
-  }
+
+function validateRegistration(){
+    if (passwordInput.value != passwordInputConfirm.value) {
+      register_confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        register_confirm_password.setCustomValidity('');
+    }
+
+    if (!registerCheckbox.checked) {
+      registerCheckbox.setCustomValidity("The checkbox's nescessary");
+    } else {
+        registerCheckbox.setCustomValidity('');
+    }
 }
-// register_password.onchange = validateRegistration;
-// register_confirm_password.onkeyup = validateRegistration;
+register_password.onchange = validateRegistration;
+register_confirm_password.onkeyup = validateRegistration;
+
 
 function registerButton() {
     const bodyRegister = {
@@ -38,15 +41,95 @@ function registerButton() {
 }
 
 registerSubmitButton.addEventListener('click', function() {
-        (passwordInput.value == passwordInputConfirm.value) && (registerCheckbox.checked == true) ? registerButton() : validateRegistration();
+    (passwordInput.value == passwordInputConfirm.value) && (registerCheckbox.checked == true) ? registerButton() : validateRegistration();
 });
 registerCheckbox.onclick = () => {
     registerCheckbox.setCustomValidity('');
 }
-// Password registration validation form ends here
 
-// Password login validation form
 
+// TODO: cookies deletion function;
+
+//cookies
+inputCheckbox.onclick = () => {
+    if (loginPassword.value && loginEmail.value) {
+      setCookie(loginEmail.value, loginPassword.value, 365);
+    } else {
+      inputCheckbox.checked = false;
+    }
+}
+
+function pastePassword() {
+    let c = document.cookie;
+    let d = loginEmail.value;
+    let ans = ''
+    for (let i = 0; i<c.length; i++) {
+        if (c[i] == '=' || c[i] == ';') {
+            if (c[i] == '=') {
+              ans += ' ';
+            }
+            if (c[i] == ';') {
+              ans += '';
+            }
+        } else {
+            ans += c[i];
+        }
+    }
+    let j = ans.split(' ');
+    let p = j[j.indexOf(d) + 1];
+    loginPassword.value = p;
+}
+
+loginPassword.onclick = () => {
+    let x = checkCookie();
+    x == true ? pastePassword() : console.log('failed');
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+function checkCookie() {
+  if (loginEmail.value) {
+    var user = getCookie(`${loginEmail.value}`);
+    if (user != "") {
+      return true;
+    } else {
+      console.log("memorizing user");
+      user = loginEmail.value;
+      if (loginPassword.value) {
+          if (user != "" && user != null) {
+            setCookie(user, loginPassword.value, 30);
+          }
+      } else {
+        validateLogin();
+      }
+    }
+  } else {
+    validateLogin();
+  }
+}
+
+//cookies ends here
 
 
 var x = document.getElementById("login");
