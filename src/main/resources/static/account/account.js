35f1e95bce0bbde967e8f83b9bfcfb4f62e22fbc
dreamvibe1993 <dreamvibe@mail.ru>
2020-10-10 22:07:02 +0500

    messages linebraking algotirhm improved
    пиздец блять!

--- src/main/resources/static/account/account.js
+++ src/main/resources/static/account/account.js
@@ -70,36 +70,80 @@ techCross.onclick = () => {
 // techsupport modal summoner code
 
 // techsupport modal chatwindow - inputfield interaction code
-
 inptContainer.onclick = () => {
   if (inptContainer.innerHTML == 'Type here') {
     inptContainer.innerHTML = '';
-    inptContainer.addEventListener('keydown', function(e) {
-    	if (!e.shiftKey && e.keyCode === 13) {
-        //str transform block
-        var str = inptContainer.innerText;
-        var ans = '';
-        for (let i = 0; i <= str.length; i++) {
-          if (i == 10) {
-            ans += `${str.slice(0, i)}\n`;
-            str =  `${str.slice(i)}`;
-            i -= i;
-            }
-            inptContainer.innerText = `${ans}${str}`;
-        }
 
-        // TODO: spacekey control + window cursor out
-
-        //str transform block
-      	console.log('sending a message');
-        chatMsgWindow.insertAdjacentHTML('beforeend', `<div class="chat-second-line"><div id="clientMsg" class="message">${inptContainer.innerText}</div></div>`);
-        inptContainer.innerHTML = '';
-        chatMsgWindow.scrollTop += chatMsgWindow.clientHeight;
-        e.preventDefault();
-      }
-    });
   }
+}
+
+sendPlane.onmousedown = () => {
+  let finale = false;
+      var str = inptContainer.innerText;
+      var msgwidth = document.getElementById('suppMsg').offsetWidth;
+      let n = Math.round(msgwidth / 8.6);
+      var ans = '';
+      let arr = str.split(' ');
+      if (str.length <= n) {
+        ans += str;
+      } else {
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].length > n) {
+            let o = `${arr[i].slice(0, n)}`
+            arr.splice(i, 0, o);
+            arr[i+1] = `${arr[i+1].slice(n)}`;
+          }
+        }
+        let i = 0;
+        if (arr[i].length <= n) {
+
+          while (arr[i].length != 0) {
+            if (arr[i].length < n) {
+              if (arr[i + 1] == undefined) {
+                ans += `${arr[i]}\n`;
+                arr[i] = '';
+              } else {
+                arr[i+1] = `${arr[i]} ${arr[i + 1]}`;
+                arr.shift();
+              }
+            }
+            if (arr[i].length == n) {
+              ans += `${arr[i]}\n`;
+              arr.shift();
+            }
+            if (arr[i].length > n) {
+              if (arr[i + 1] == undefined) {
+                let lastspace = arr[i].lastIndexOf(' ');
+                ans += `${arr[i].slice(0, lastspace)}\n${arr[i].slice(lastspace+1)}`;
+                arr[i] = '';
+              } else {
+                let lastspace = arr[i].lastIndexOf(' ');
+                ans += `${arr[i].slice(0, lastspace)}\n`;
+                arr[i+1] = `${arr[i].slice(lastspace+1)} ${arr[i + 1]}`;
+                arr.shift();
+              }
+            }
+          }
+        }
+        finale = ans;
+    }
+
+
+          //str transform block
+      if (finale && inptContainer.innerHTML != "Type here") {
+      console.log('sending a message');
+      chatMsgWindow.insertAdjacentHTML('beforeend', `<div class="chat-second-line"><div id="clientMsg" class="message">${finale}</div></div>`);
+      inptContainer.innerHTML = '';
+      chatMsgWindow.scrollTop += chatMsgWindow.clientHeight;
+      // e.preventDefault();
+    }
+
 };
+
+sendPlane.onmouseup = () => {
+  finale = false;
+}
+
 
 // techsupport modal chatwindow - inputfield interaction code
 
@@ -149,50 +193,50 @@ transformAccField();
 
 // tips account frame code
 
-let tooltip;
-
-document.onmouseover = function(event) {
-
-  let anchorElem = event.target.closest('[data-tip]');
-
-  if (!anchorElem) return;
-
-  tooltip = showTooltip(anchorElem, anchorElem.dataset.tip);
-}
-
-document.onmouseout = function() {
-
-  if (tooltip) {
-    tooltip.remove();
-    tooltip = false;
-  }
-
-}
-
-
-function showTooltip(anchorElem, data) {
-
-
-  let tooltipElem = document.createElement('div');
-  tooltipElem.className = 'tooltip';
-  tooltipElem.innerHTML = data;
-
-  document.body.append(tooltipElem);
-
-  let coords = anchorElem.getBoundingClientRect();
-
-  let left = coords.left + (anchorElem.offsetWidth - tooltipElem.offsetWidth) / 2;
-  if (left < 0) left = 0;
-
-  let top = coords.top + anchorElem.offsetHeight + 5;
-  if (top < 0) {
-    top = coords.top + anchorElem.offsetHeight + 5;
-  }
-
-  tooltipElem.style.left = left + 'px';
-  tooltipElem.style.top = top + 'px';
-
-  return tooltipElem;
-}
+// let tooltip;
+//
+// document.onmouseover = function(event) {
+//
+//   let anchorElem = event.target.closest('[data-tip]');
+//
+//   if (!anchorElem) return;
+//
+//   tooltip = showTooltip(anchorElem, anchorElem.dataset.tip);
+// }
+//
+// document.onmouseout = function() {
+//
+//   if (tooltip) {
+//     tooltip.remove();
+//     tooltip = false;
+//   }
+//
+// }
+//
+//
+// function showTooltip(anchorElem, data) {
+//
+//
+//   let tooltipElem = document.createElement('div');
+//   tooltipElem.className = 'tooltip';
+//   tooltipElem.innerHTML = data;
+//
+//   document.body.append(tooltipElem);
+//
+//   let coords = anchorElem.getBoundingClientRect();
+//
+//   let left = coords.left + (anchorElem.offsetWidth - tooltipElem.offsetWidth) / 2;
+//   if (left < 0) left = 0;
+//
+//   let top = coords.top + anchorElem.offsetHeight + 5;
+//   if (top < 0) {
+//     top = coords.top + anchorElem.offsetHeight + 5;
+//   }
+//
+//   tooltipElem.style.left = left + 'px';
+//   tooltipElem.style.top = top + 'px';
+//
+//   return tooltipElem;
+// }
 
 // tips account frame code ends here
